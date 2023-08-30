6d5da54331512327fbd2a9a524805c75ec3d54bb
Jacek Kaczmarek <infinite.tech.poznan@gmail.com>
2023-08-30 14:26:01 +0200

    feat: add jasypt configuration

--- debug.sh
+++ debug.sh
@@ -1,6 +1,10 @@
 #git pull
 #export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
-mvn clean install
+mvn jasypt:decrypt -Djasypt.encryptor.password=CodePredXXAA12
+mvn clean install -Djasypt.encryptor.password=CodePredXXAA12
 docker-compose down
 clear
+
+set -a
+source .my-env
 docker-compose up --build
